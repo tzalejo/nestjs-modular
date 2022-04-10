be6ae70439855999325f61822c5a35946f69e717
Alejandro Valenzuela <tzalejo@gmail.com>
2022-04-10 00:33:04 -0300

    agregamos modulo globales

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
-const API_KEY='123456';
-const API_KEY_PROD='admin';
-
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule],
+  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV=== 'prod' ? API_KEY_PROD: API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
