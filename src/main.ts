72f162218961d3ac3ef4b9450b7dfe40981993cf
Alejandro Valenzuela <tzalejo@gmail.com>
2022-04-15 02:34:37 -0300

    agrego swagger para documentar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +12,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('PLATZI STORE')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3100);
 }
 bootstrap();
