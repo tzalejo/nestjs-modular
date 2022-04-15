7af1dce4695f5422462ee9df16bf91730876142b
Alejandro Valenzuela <tzalejo@gmail.com>
2022-04-15 01:07:33 -0300

    configuracion de tipado seguro

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,16 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
-import {enviroments} from './enviroments';
+import {enviroments } from './enviroments';
+import config from './config';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule, 
+  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule,
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [
